98ef112970ec1a8ff5d57f907db715db696d874c
Nir <59836189+nirsaban@users.noreply.github.com>
2021-02-05 14:02:04 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import {Switch,Route} from "react-router-dom";
 import HomePage from './pages/homepage/homepage.component.jsx';
-import ShopPage from './pages/shop/shop.component'
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.compomnent.jsx'
 import './App.css';
-const Hats = () =>(
-<div>
-<h1>Hats</h1>
-</div>
-)
 
 class App extends React.Component {
  
   render() {
     return (
       <div>
+      <Header />
       <Switch>
       <Route exact path ='/' component = {HomePage} />
       <Route exact path ='/shop' component = {ShopPage} />
       </Switch>
+      
       </div>
     );
   }
